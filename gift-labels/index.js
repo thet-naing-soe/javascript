f90caeb5e3922c4efb6a8809eadecdcd8192f7d8
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2024-11-26 15:55:08 +0630

    gift labels

---
+++ gift-labels/index.js
@@ -0,0 +1,25 @@
+function getLabelsHtml(text, sender, ...staffObj) {
+  return staffObj
+    .map(
+      (staff) =>
+        `<div class="label-card">
+        <p>Dear ${staff.name} </p>
+        <p>${text}</p>
+        <p>Best wishes,</p>
+        <p>${sender}</p>
+      </div>`
+    )
+    .join("");
+}
+
+const text = "Thank you for all your hard work throughout the year! 🙏🎁";
+const sender = "Tom";
+
+document.getElementById("labels-container").innerHTML = getLabelsHtml(
+  text,
+  sender,
+  { name: "Sally" },
+  { name: "Mike" },
+  { name: "Rob" },
+  { name: "Harriet" }
+);
