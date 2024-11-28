9ba6c98b88b3f1a5b29f53ce04ec5d6985a29480
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2024-11-28 13:07:18 +0630

    Handling Rejected Promises

---
+++ handling-rejected-promises/index.js
@@ -0,0 +1,15 @@
+const getHandlingPromise = async () => {
+  try {
+    const response = await fetch("ttps://dog.ceo/api/breeds/image/random");
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    console.log("The operation completed.");
+  }
+};
+getHandlingPromise();
