e8cddb834f75a60a5f19e3e5fed7f20ea874c20b
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2024-11-28 13:45:09 +0630

    Updating Handling Rejected Promises

--- handling-rejected-promises/index.js
+++ handling-rejected-promises/index.js
@@ -1,6 +1,19 @@
 const getHandlingPromise = async () => {
   try {
-    const response = await fetch("https://dog.ceo/api/breeds/image/random");
+    const response = await fetch(
+      "https://apis.scrimba.com/jsonplaceholder/posts",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          title: "Holiday Nightmares",
+          body: "When I was kidnapped in Scotland…",
+          userID: 100,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
