3080a562768867abea309c695ac77ef9fe0d1bbd
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2024-12-03 08:34:23 +0630

    closure

--- closure/index.js
+++ closure/index.js
@@ -1,15 +1,3 @@
-/* 
-- The account balance should start at zero.
-
-- The function should return an object containing 
-  3 functions. One for depositing money, one for 
-  withdrawing money, and a getBalance function which 
-  logs the balance and the account holder's name.
-
-- Deposits should add to the balance, and withdrawals should subtract 
-  from it. 
-*/
-
 function createBankAccount(name) {
   let balance = 0;
   return {
